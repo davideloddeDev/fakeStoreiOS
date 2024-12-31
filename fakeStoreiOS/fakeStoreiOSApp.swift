0db9510f4dda31313552679e93812eaf4a68542b
Davide Lodde <johndoe@example.com>
2024-12-31 10:50:22 +0100

    Initial Commit

---
+++ fakeStoreiOS/fakeStoreiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  fakeStoreiOSApp.swift
+//  fakeStoreiOS
+//
+//  Created by davide  lodde  on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct fakeStoreiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
